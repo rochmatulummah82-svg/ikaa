2fbd3e1c2861f5fd5254946aadcee348d8398ae6
rochmatulummah82-svg <rochmatulummah82@gmail.com>
2025-09-30 14:26:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
   <a href='___'>Kunjungi Instagram Saya</a>
 <button id='sapaButton'>Sapa Saya!</button>
   </div>
+//1. Pilih tombol berdasarkan ID-nya
+const tombolSapa = document.getElementByid('sapaButton');
+//2. Tambahkan 'event listener' untuk aksi 'click'
+tombolSapa.addEventListener('click, function() )
+//3. Jalankan kkode ini ketika tombol di-klik
+  alert('hello! Terima kasih sudah berkunjug!')'
+)):
